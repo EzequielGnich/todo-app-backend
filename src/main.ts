aecae7e338b4aac21c99269d89e3ba1765819f36
Ezequiel Gnich <ezequiel.gnich@syngenta.com>
2024-01-13 22:46:30 -0300

    feat: Implementing app entry point

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+      .setTitle('Todos')
+      .setDescription('The Todos API')
+      .setVersion('0.1')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+
+    await app.listen(3000);
+  } catch (err) {
+    console.log(err);
+    process.exit();
+  }
+}
+
+bootstrap();
