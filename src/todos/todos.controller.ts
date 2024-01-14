93797e16b290713bca88b72307ade62108494610
Ezequiel Gnich <ezequiel.gnich@syngenta.com>
2024-01-13 23:41:58 -0300

    feat: Implementing todos

---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Headers,
+} from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { JwtService } from '@nestjs/jwt';
+
+@Controller('todos')
+export class TodosController {
+  constructor(
+    private readonly todosService: TodosService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  @Post()
+  create(@Body() createTodoDto: CreateTodoDto) {
+    return this.todosService.create(createTodoDto);
+  }
+
+  @Get()
+  findAllByUser(@Headers('authorization') authHeader: string) {
+    const user = this.jwtService.decode(authHeader.split(' ')[1]);
+    return this.todosService.findAllByUser(user);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.todosService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+    return this.todosService.update(id, updateTodoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.todosService.remove(id);
+  }
+}
---
+++ src/todos/todos.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class TodosService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createTodoDto: CreateTodoDto) {
+    return this.prisma.todo.create({ data: createTodoDto });
+  }
+
+  findAllByUser(userId: any) {
+    console.log({ userId });
+    return this.prisma.todo.findMany({ where: { userId: userId.sub } });
+  }
+
+  findOne(id: string) {
+    return this.prisma.todo.findUnique({ where: { id } });
+  }
+
+  update(id: string, updateTodoDto: UpdateTodoDto) {
+    return this.prisma.todo.update({
+      where: { id },
+      data: updateTodoDto,
+    });
+  }
+
+  remove(id: string) {
+    return this.prisma.todo.delete({ where: { id } });
+  }
+}
