0bc55cb37d08b39c41a704bf420f6f2945a739c7
Ezequiel Gnich <ezequiel.gnich@syngenta.com>
2024-01-13 21:20:45 -0300

    feat: First configurations and libs

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+export default config;
