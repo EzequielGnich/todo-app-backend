0d1982a14a33f92dd2a98462db04474788c5f296
Ezequiel Gnich <ezequiel.gnich@syngenta.com>
2024-01-15 13:14:38 -0300

    fix: Adjust jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -2,12 +2,15 @@ import type { Config } from 'jest';
 
 const config: Config = {
   verbose: true,
-  roots: ['<rootDir>/src'],
+  rootDir: 'src',
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.(t|j)s$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
 };
 
 export default config;
