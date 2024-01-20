a74edfb34ba5169afb62a3afd0065943897a66ef
Ezequiel Gnich <ezequiel.gnich@syngenta.com>
2024-01-20 12:24:30 -0300

    chore: Added packages of security

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,29 @@ import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { TodosModule } from './todos/todos.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerConfigService } from './common/throttler/throttler.service';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, TodosModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    TodosModule,
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: ThrottlerConfigService
+    })
+  ],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: APP_GUARD,
-      useClass: JwtAuthGuard,
+      useClass: JwtAuthGuard
     },
-  ],
+    ThrottlerConfigService
+  ]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,34 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
 
-    const config = new DocumentBuilder()
-      .setTitle('Todos')
-      .setDescription('The Todos API')
-      .setVersion('0.1')
-      .build();
+    app.use(helmet());
+
+    app.enableCors({
+      origin: false // Specify the allowed origins.  I'm setting false to allow requests from any origin
+      // Find more configuration options here: https://github.com/expressjs/cors#configuration-options
+    });
+
+    const config = new DocumentBuilder().setTitle('Todos').setDescription('The Todos API').setVersion('0.1').build();
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
+    // -- Validation
     app.useGlobalPipes(
       new ValidationPipe({
         transform: true,
         whitelist: true,
         forbidNonWhitelisted: true,
-      }),
+        transformOptions: {
+          enableImplicitConversion: true
+        }
+      })
     );
 
     await app.listen(process.env.PORT || 3000);
